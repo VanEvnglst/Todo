d72ede3120a6ff9fca5340093e7d1d6b4ae43d8a
Ivan <engr.ivan.evangelista@gmail.com>
2023-01-25 10:03:52 +0800

    rename file to main screen

--- src/screens/main-screen.tsx
+++ src/screens/main-screen.tsx
@@ -1,8 +1,15 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
 import { Text, Box, Center, VStack, themeTools, useTheme, useColorMode, useColorModeValue } from 'native-base';
 import ThemeToggle from '../components/theme-toggle';
+import TaskItem from '../components/task-item';
 
 export default function MainScreen() {
+  const [checked, setChecked] = useState(false)
+
+  const handlePressCheckBox = useCallback(() => {
+    setChecked(prev => !prev)
+  }, []);
+
   return (
     <Center
       _dark={{ bg: 'blueGray.900' }}
@@ -12,6 +19,7 @@ export default function MainScreen() {
     >
       <VStack space={5} alignItems='center'>
         <Box p={10} bg={useColorModeValue('red.500', 'yellow.500')}>
+          {/* <TaskItem isDone={checked} onToggleCheckbox={handlePressCheckBox}/> */}
           <Text>Hello</Text>
         </Box>
         <ThemeToggle />
